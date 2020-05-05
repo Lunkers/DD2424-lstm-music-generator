97864e7b2e07e1107bbd8824ba79b8588ffdba83
Fredrik Lundkvist <flundkvi@kth.se>
2020-05-05 15:03:42 +0200

    define model, add data processing, one-hot encodin/decoding

---
+++ model.py
@@ -0,0 +1,39 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTM, self).__init__()
+
+        self.hidden_size = hidden_size
+        #input gate weights
+        self.Wi = nn.Linear(hidden_size, hidden_size, bias=True)
+        self.Ui = nn.Linear(input_size, hidden_size, bias=True)
+        #Forget gate weights
+        self.Wf = nn.Linear(hidden_size, hidden_size,bias=True)
+        self.Uf = nn.Linear(input_size, hidden_size, bias=True)
+        #output gate weights
+        self.Wo = nn.Linear(hidden_size, hidden_size, bias=True)
+        self.Uo = nn.Linear(input_size, hidden_size, bias=True)
+        #cell weights
+        self.Wc = nn.Linear(hidden_size,hidden_size, bias=True)
+        self.Uc = nn.Linear(input_size, hidden_size, bias=True)
+
+        #Fully connected layer for output
+        self.V = nn.Linear(hidden_size, output_size, bias=True)
+    
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+    def initMemory(self):
+        return torch.zeros(1, self.hidden_size)
+
+    def forward(self,input_data, hidden, memory):
+        i = F.sigmoid(self.Wi(hidden) + self.Ui(input_data))
+        f = F.sigmoid(self.Wf(hidden) + self.Uf(input_data))
+        o = F.sigmoid(self.Wo(hidden) + self.Uo(input_data))
+        c_tilde = F.tanh(self.Wc(hidden) + self.Uc(input_data))
+        c = f * memory + i * c_tilde
+        h = o * tanh(c)
+
+        output = F.softmax(self.V(h))
+        return output, h, c
--- processing.py
+++ processing.py
@@ -1,7 +1,9 @@
 from music21 import converter, instrument, note, chord
 import json
+import torch
 
-
+nextString = "<NEXT>"
+endString = "<END>"
 
 
 def load_data():
@@ -15,19 +17,82 @@ def load_data():
                 file_arr.append(row['midi_filename'])
         
    
-        for file_name in file_arr[30:40]:
-            midi = converter.parse(dir_prefix + file_name)
-            print("Parsing %s", file_name)
-            with open("notes.txt", 'a') as notefile:
-                for element in midi.flat.notes:
-                    if isinstance(element, note.Note):
-                        notefile.write(str(element.pitch))
-                    if isinstance(element, chord.Chord):
-                        notefile.write('.'.join(str(n) for n in element.normalOrder))
-        #print(len(set(notes)))
-        #A5.C5.D5 ->[0 0 0 .. 1 0 ...] -> A5.C5.D5 -> chord.Chord(letter.split("."))
-        # x[i] -> y[i+1] ≠ x[i+1]
-        # D -> x = D[i:i+seq_length], y = D[i +1: i + seq_length]
-        #A5.C5.D5 ->[0 0 0 .. 1 0 .. 1], [0 0 0 .. 0 1 .. 1] -> A5.C5.D5 ->
-        #0.3.7 -> färre dimensioner, kanske inte lika träffsäkert
+    #parse
+    #lägg in alla noter i vektor, <NEXT> där det behövs
+    #i slutet av filen , lägg till en <END>-token
+    #när alla filer parsade, returnera notes
+    notes = []
+
+    for file_name in file_arr:
+        midi = converter.parse(dir_prefix + file_name)
+        print("Parsing {0}".format(file_name))
+        for element in midi.flat.notes:
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            if isinstance(element, chord.Chord):
+                for n in element.pitches:
+                    notes.append(str(n))
+            notes.append(nextString)
+        notes.append(endString)
+
+    data = {"notes":notes}
+
+
+    #spara notes som JSON-fil
+    #JSON.dumps(s)
+    with open("notes.json", 'a') as notefile:
+        json.dump(data, notefile)
+
+def read_data(file_path):
+    data = JSON.load(file_path)
+    data = data["notes"]
+    alphabet = set(data)
+    sign_to_int = dict([(y,x) for x,y in enumerate(sorted(alphabet))])
+    int_to_sign = dict([(x,y) for x,y in enumerate(sorted(alphabet))])
+
+    return sign_to_int, int_to_sign
+
+
+def one_hot_encoding(sign_int: int, alphabet_size: int):
+
+    """get one-hot encoded tensor for a sign in an alphabet
+        Args:
+            sign_int: sign in alphabet converted to integer value
+            alphabet_size: total size of alphabet
+        
+        Returns:
+            a one-hot encoded tensor of size [1 x alphabet_size]
+    """
+
+    one_hot = torch.zeros(1, alphabet_size)
+    one_hot[0][sign_int] = 1
+    return one_hot
+    
+def decode_output(prob_vec:torch.tensor, int_to_sign: dict):
+    """
+        Get a sign in our alphabet from a prboability vector
+
+        Args:
+            prob_vec: a tensor of size [1 x alphabet size], containing probabilities for each sign
+            int_to_sign: a dictionary containing mappings from indices (ints) to signs 
+
+        Returns:
+            The sign with the highest probability
+
+    """
+    _ , index = prob_vec.max(1)
+    return int_to_sign[index[0].item()]
+    
+def build_midi():
+    pass
+
+
+
+
 load_data()
+sign2int, int2sign = read_data('./notes.json')
+with open("sign2int.json") as f:
+    json.dump(sign2int, f)
+
+with open("int2sign.json") as f:
+    json.dump(int2sign, f)
