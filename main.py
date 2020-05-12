971d545add738207b1eaaaef02bb9b2d56275729
Fredrik Lundkvist <flundkvi@kth.se>
2020-05-12 15:18:39 +0200

    set up adagrad

--- main.py
+++ main.py
@@ -27,15 +27,15 @@ def main():
 
     network = LSTM(hidden_size = 64, input_size = 90, output_size = 90)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(network.parameters(), learning_rate)
+    optimizer = optim.Adagrad(network.parameters(), learning_rate)
     # move network to GPU
     network.to(device)
     print(device)
-    network, _, losses = trainLoop(network, criterion, notes, optimizer, 2, seq_length, sign_to_int)
+    network, _, losses = trainLoop(network, criterion, notes[1:10000], optimizer, 2, seq_length, sign_to_int)
     plt.figure()
     plt.plot(losses)
     plt.savefig('losses.png')
-    acc = evaluateAccuracy(validation, network, seq_length, sign_to_int)
+    acc = evaluateAccuracy(validation[1:10000], network, seq_length, sign_to_int)
     print(acc)
 
     save_network(network, "net.pth")
@@ -101,10 +101,7 @@ def train(network: LSTM, criterion, input_seq, follow_seq, optimizer: optim.Opti
         output, hidden, memory = network(input_seq[i], hidden, memory)
         l =  criterion(output, follow_seq[i])
         #TODO: smooth loss here
-        if i == 0:
-            loss +=l
-        else:
-            loss = 0.999 * loss + 0.001 * loss
+        loss += l
     
         
     loss.backward()
